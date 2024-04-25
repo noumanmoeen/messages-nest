f5b72c623fb7ac4893a317234e260fbcc22a669f
moeennouman7@gmail.com <moeennouman7@gmail.com>
2024-04-25 22:30:31 +0500

    - Added messages controller

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+
+    @Get()
+    getMessages(){
+
+    }
+
+    @Get('/:id')
+    getMessageById(){
+
+    }
+
+    @Post()
+    createMessage(){
+
+    }
+
+}
