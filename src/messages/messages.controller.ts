95302a6a8be47e78b6e3d75eb7b62e0950645b21
moeennouman7@gmail.com <moeennouman7@gmail.com>
2024-04-27 23:27:19 +0500

    Refactoring code

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -2,22 +2,18 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
+  @Get()
+  getMessages() {
+    return ['Hi', 'Bye'];
+  }
 
-    @Get()
-    getMessages(){
-        return ["Hi", 'Bye']
-    }
+  @Get('/:id')
+  getMessageById(@Param() param: any) {
+    console.log('PARAM ________ ', param);
+  }
 
-    @Get('/:id')
-    getMessageById(@Param() param : any){
-
-        console.log('PARAM ________ ',param)
-
-    }
-
-    @Post()
-    createMessage(@Body() body : any){
-        console.log('BODY ______', body)
-    }
-
+  @Post()
+  createMessage(@Body() body: any) {
+    console.log('BODY ______', body);
+  }
 }
