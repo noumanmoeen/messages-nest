4563b26ed84151200d45d8cbf7a87c656abf49d8
moeennouman7@gmail.com <moeennouman7@gmail.com>
2024-04-28 20:39:06 +0500

    Added CreateMessageDto and added it to create message requrest controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -13,7 +14,7 @@ export class MessagesController {
   }
 
   @Post()
-  createMessage(@Body() body: any) {
+  createMessage(@Body() body: CreateMessageDto) {
     console.log('BODY ______', body);
   }
 }
